5a18c166cda41b6b0f7312f790879309b4a13060
TurkiAlorwan <129651948+TurkiAlorwan@users.noreply.github.com>
2023-06-04 06:04:30 +0300

    Update models.py

--- PhoneStore/models.py
+++ PhoneStore/models.py
@@ -7,8 +7,8 @@ class Phone(models.Model):
     manufacturer = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
-  #  image = models.ImageField(
-      #  upload_to='https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-14-pro-max-1.jpg')
+    image = models.ImageField(
+       upload_to='https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-14-pro-max-1.jpg')
 
     def str(self):
         return f"{self.name} {self.price} {self.manufacturer} {self.description}"
